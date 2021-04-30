79ff31c2d33df9fb0e61b1598153e3350b58cb12
KJay221 <k20002216408@gmail.com>
2021-04-30 20:32:41 +0800

    feat: delete get money

--- api/endpoints/money/__init__.py
+++ api/endpoints/money/__init__.py
@@ -1,16 +1,8 @@
 from endpoints.classes import Resource
 
 from .add_record import add_record
-from .get_record import get_record
 
 MONEY = [
-    Resource(
-        "GET",
-        "/money",
-        get_record,
-        "Used for get money record",
-        "Get money record",
-    ),
     Resource(
         "POST",
         "/money",
