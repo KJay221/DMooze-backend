f2105395b47857b7894394e2905c19a76e440857
KJay221 <k20002216408@gmail.com>
2021-05-16 19:14:27 +0800

    feat: can delete all proposal

--- api/endpoints/proposal/delete.py
+++ api/endpoints/proposal/delete.py
@@ -2,11 +2,28 @@ from fastapi.responses import PlainTextResponse
 from loguru import logger
 
 from db import SESSION
-from models import Proposal
+from models import ImageList, MoneyList, Proposal, WithdrawalList
 
 
 async def delete(proposal_id: int):
     try:
+        for element in enumerate(
+            SESSION.query(ImageList).filter(ImageList.proposal_id == proposal_id).all()
+        ):
+            SESSION.delete(element[1])
+            SESSION.commit()
+        for element in enumerate(
+            SESSION.query(WithdrawalList)
+            .filter(WithdrawalList.proposal_id == proposal_id)
+            .all()
+        ):
+            SESSION.delete(element[1])
+            SESSION.commit()
+        for element in enumerate(
+            SESSION.query(MoneyList).filter(MoneyList.proposal_id == proposal_id).all()
+        ):
+            SESSION.delete(element[1])
+            SESSION.commit()
         db_proposal = (
             SESSION.query(Proposal).filter(Proposal.proposal_id == proposal_id).first()
         )
